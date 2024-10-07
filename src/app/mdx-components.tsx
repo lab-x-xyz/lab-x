3ab9768859dfbf678c8f2f640b444b409f83ed89
Shashivadan <shashivadan99@gmail.com>
2024-10-07 14:51:36 +0530

    feat: added cloudflare worker builders

--- src/app/mdx-components.tsx
+++ src/app/mdx-components.tsx
@@ -1,5 +1,7 @@
 import type { MDXComponents } from "mdx/types";
 
+
+export const runtime = "edge"
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
